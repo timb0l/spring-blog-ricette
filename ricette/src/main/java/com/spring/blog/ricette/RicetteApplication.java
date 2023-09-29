2a5401a649b0386c61c3a19d909f35735a1a5f57
Jam Timbol <jam.timbol00@gmail.com>
2023-09-29 10:02:44 +0200

    Initiate Project

---
+++ ricette/src/main/java/com/spring/blog/ricette/RicetteApplication.java
@@ -0,0 +1,13 @@
+package com.spring.blog.ricette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RicetteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RicetteApplication.class, args);
+	}
+
+}
---
+++ ricette/src/test/java/com/spring/blog/ricette/RicetteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.blog.ricette;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RicetteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
